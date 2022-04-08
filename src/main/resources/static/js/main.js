026524532fec167eefeb6b3eb6a37190ebf971f5
Eric Opoku <eric.opoku20@yahoo.com>
2022-04-07 21:58:39 -0400

    Added basic template files with webjars

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,28 @@
+
+(function ($) {
+    $.toggleShowPassword = function (options) {
+        const settings = $.extend({
+            field: "#password",
+            control: "#toggle_show_password",
+        }, options);
+
+        const control = $(settings.control);
+        const field = $(settings.field);
+
+        control.bind('click', function () {
+            if (control.is(':checked')) {
+                field.attr('type', 'text');
+            } else {
+                field.attr('type', 'password');
+            }
+        })
+    };
+}(jQuery));
+
+$(document).ready(function () {
+    $.toggleShowPassword({
+        field: '#password',
+        control: "#showPassword"
+    });
+
+});
