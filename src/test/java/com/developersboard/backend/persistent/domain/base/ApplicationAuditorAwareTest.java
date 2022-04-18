854e569ff9d724dd054c27fec8f4b141055b78d0
Eric Opoku <20393990+ericus20@users.noreply.github.com>
2022-04-18 19:16:31 -0400

    Updating auditor aware to check for authenticated user (#36)
    * Updating auditor aware to check for authenticated user
    
    * commit badge
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/test/java/com/developersboard/backend/persistent/domain/base/ApplicationAuditorAwareTest.java
+++ src/test/java/com/developersboard/backend/persistent/domain/base/ApplicationAuditorAwareTest.java
@@ -1,17 +1,52 @@
 package com.developersboard.backend.persistent.domain.base;
 
+import com.developersboard.TestUtils;
+import java.util.Optional;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 class ApplicationAuditorAwareTest {
 
   private static final String SYSTEM_USER = "system";
 
+  @BeforeEach
+  void setUp() {
+    SecurityContextHolder.clearContext();
+  }
+
   @Test
   void getCurrentAuditor() {
     var centralizedAuditorAware = new ApplicationAuditorAware();
     Assertions.assertEquals(SYSTEM_USER, centralizedAuditorAware.getCurrentAuditor().orElse(null));
+  }
+
+  @Test
+  void getCurrentAuditorWithNoAuthentication() {
+    TestUtils.setAuthentication(TestUtils.ANONYMOUS_ROLE, TestUtils.ANONYMOUS_USER);
+    SecurityContextHolder.getContext().getAuthentication().setAuthenticated(false);
+    Assertions.assertEquals(SYSTEM_USER, getAuditor());
+  }
+
+  @Test
+  void getCurrentAuditorWithAnonymousUser() {
+    TestUtils.setAuthentication(TestUtils.ANONYMOUS_USER, TestUtils.ANONYMOUS_ROLE);
+    Assertions.assertEquals(SYSTEM_USER, getAuditor());
+  }
+
+  @Test
+  void getCurrentAuditorWithAuthenticatedUser(TestInfo testInfo) {
+    TestUtils.setAuthentication(testInfo.getDisplayName(), TestUtils.ROLE_USER);
+    Assertions.assertEquals(getAuditor(), testInfo.getDisplayName());
+  }
+
+  private String getAuditor() {
+    var applicationAuditorAware = new ApplicationAuditorAware();
+    Optional<String> currentAuditor = applicationAuditorAware.getCurrentAuditor();
+    return currentAuditor.orElse(null);
   }
 
   @Test
