582b8bd1116f546cc934d355f3b5eeae70b0fabd
Eric Opoku <eric.opoku20@yahoo.com>
2022-07-13 10:21:08 -0400

    feat: display admin credentials

--- src/main/java/com/developersboard/web/controller/HomeController.java
+++ src/main/java/com/developersboard/web/controller/HomeController.java
@@ -2,7 +2,9 @@ package com.developersboard.web.controller;
 
 import com.developersboard.constant.HomeConstants;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,13 +20,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(HomeConstants.INDEX_URL_MAPPING)
 public class HomeController {
 
+  @Value("${admin.username}")
+  private String adminUsername;
+
+  @Value("${admin.email}")
+  private String adminEmail;
+
+  @Value("${admin.password}")
+  private String adminPassword;
+
   /**
    * Maps index url request.
    *
    * @return index view name
    */
   @GetMapping
-  public String home() {
+  public String home(Model model) {
+    model.addAttribute("adminUsername", adminUsername);
+    model.addAttribute("adminEmail", adminEmail);
+    model.addAttribute("adminPassword", adminPassword);
+
     return HomeConstants.INDEX_VIEW_NAME;
   }
 }
