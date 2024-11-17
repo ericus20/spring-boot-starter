03dc1f81d0432fbfe80c13248d5ed94c282818da
Eric Opoku <20393990+ericus20@users.noreply.github.com>
2024-11-17 16:08:02 -0500

    silent exception when admin user already exists (#100) (#101)
    Co-authored-by: Eric Opoku <ericus20@macbookpro.mynetworksettings.com>

--- src/main/java/com/developersboard/backend/bootstrap/DatabaseSeeder.java
+++ src/main/java/com/developersboard/backend/bootstrap/DatabaseSeeder.java
@@ -5,23 +5,26 @@ import com.developersboard.backend.service.user.RoleService;
 import com.developersboard.backend.service.user.UserService;
 import com.developersboard.constant.EnvConstants;
 import com.developersboard.enums.RoleType;
+import com.developersboard.exception.user.UserAlreadyExistsException;
 import com.developersboard.shared.util.UserUtils;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 /**
- * A convenient class to initializes and save user data on application start.
+ * A convenient class to initialize and save user data on application start.
  *
  * @author George Anguah
  * @version 1.0
  * @since 1.0
  */
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class DatabaseSeeder implements CommandLineRunner {
@@ -50,9 +53,13 @@ public class DatabaseSeeder implements CommandLineRunner {
   }
 
   private void persistDefaultAdminUser() {
-    var adminDto = UserUtils.createUserDto(adminUsername, adminPassword, adminEmail, true);
-    Set<RoleType> adminRoleType = Collections.singleton(RoleType.ROLE_ADMIN);
+    try {
+      var adminDto = UserUtils.createUserDto(adminUsername, adminPassword, adminEmail, true);
+      Set<RoleType> adminRoleType = Collections.singleton(RoleType.ROLE_ADMIN);
 
-    userService.createUser(adminDto, adminRoleType);
+      userService.createUser(adminDto, adminRoleType);
+    } catch (UserAlreadyExistsException e) {
+      LOG.warn("Admin user already exists!");
+    }
   }
 }
